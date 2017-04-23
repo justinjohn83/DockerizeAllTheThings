6d35a5fb779497e2ade28899fa9c6808e2fdc104
Justin Montgomery <jmontgomery@gamesalutes.com>
2017-04-23 15:56:01 -0500

    Adding material styling

--- angular/product-portal/src/app/app.module.ts
+++ angular/product-portal/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// Material modules and hammerjs dependency requirement for gestures
+import {MaterialModule} from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +17,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
