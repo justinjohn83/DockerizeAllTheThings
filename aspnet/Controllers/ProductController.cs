bf692e3609f31e5f4f314ea8783ad6ed2044aeea
Justin Montgomery <jmontgomery@gamesalutes.com>
2017-04-23 19:33:00 -0500

    initial ASP.NET Web application with product listing

---
+++ aspnet/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<String> GetAll()
+        {
+            return new String[] { "Pants", "Khakis", "Pizzas"}.AsEnumerable();
+        }
+    }
+}
