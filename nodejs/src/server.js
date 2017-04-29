ea062b296b38d71053c302b52a453cc14c84ad01
Justin Montgomery <jmontgomery@gamesalutes.com>
2017-04-29 14:06:11 -0500

    Adding missing semilcons

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/api/food', function (req, res) {
   var foods = ['Pizzas','Burgers','Hot Dogs'];
   console.log('Foods=' + foods);
   return res.send(foods);
-})
+});
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000!')
-})
+  console.log('Listening on port 3000!');
+});
